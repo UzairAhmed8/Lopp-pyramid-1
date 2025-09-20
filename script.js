ac0d4bb3130c57c9d88ad95a017213fd8a817ee9
UzairAhmed8 <ua5000076@gmail.com>
2025-09-20 16:44:44 +0500

    done

---
+++ script.js
@@ -0,0 +1,15 @@
+for( i = 1; i < 2 ; i++){
+    console.log( i)
+    for( j = 2; j < 3 ; j++){
+        console.log( i , j)
+        for( k = 3; k < 4 ; k++){
+            console.log( i , j , k)
+            for( l = 4; l < 5 ; l++){
+                console.log( i , j , k , l)
+                for( m = 5; m < 6 ; m++){
+                    console.log(  i , j , k , l , m)
+                }
+            }
+        }
+    }
+}
